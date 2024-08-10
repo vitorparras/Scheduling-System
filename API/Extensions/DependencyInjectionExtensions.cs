b61e8fdfeda5535a96ef559ca9e41382dd6e4c2a
Vitor Parras <vitorhugoparras@hotmail.com>
2024-08-10 17:28:24 -0300

    Organize dependency injections

--- API/Extensions/DependencyInjectionExtensions.cs
+++ API/Extensions/DependencyInjectionExtensions.cs
@@ -1,12 +1,22 @@
-﻿namespace API.Extensions
+﻿using Application.Services;
+using Application.Services.Interfaces;
+using Infrastructure;
+using Infrastructure.Repository;
+using Infrastructure.Repository.Interface;
+
+namespace API.Extensions
 {
     public static class DependencyInjectionExtensions
     {
-        public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+        public static WebApplicationBuilder ConfigureBuilder(this WebApplicationBuilder builder)
         {
             builder.Services
                   .AddEndpointsApiExplorer()
                   .AddSwaggerGen();
+
+            builder.ConfigureContext();
+            builder.ConfigureRepositories();
+            builder.ConfigureServices();
 
             return builder;
         }
@@ -20,6 +30,30 @@
             }
 
             app.UseHttpsRedirection();
+        }
+
+
+        public static WebApplicationBuilder ConfigureRepositories(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            builder.Services.AddScoped<ITokenHistoryRepository, TokenHistoryRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+            return builder;
+        }
+
+        public static WebApplicationBuilder ConfigureContext(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<DxContext>();
+
+            return builder;
+        }
+        
+        public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IAuthService, AuthService>();
+
+            return builder;
         }
     }
 }
