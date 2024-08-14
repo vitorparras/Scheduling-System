7465c12cf94af8e73efb781568f45d8af9c5f070
Vitor Parras <vitorhugoparras@hotmail.com>
2024-08-13 23:48:54 -0300

    Added generic response to services

---
+++ Domain/DTO/GenericResponse.cs
@@ -0,0 +1,34 @@
+﻿namespace Domain.DTO
+{
+    public class GenericResponse<TData>
+    {
+        public GenericResponse(IEnumerable<Erros>? erros)
+        {
+            Erros = erros;
+            Success = false;
+        }
+        
+        public GenericResponse(TData data)
+        {
+            Data = data;
+            Success = true;
+        }
+        
+        public GenericResponse(string messagemErro, bool success)
+        {
+            Erros = [new() { Message = messagemErro }];
+            Success = success;
+        }
+
+        public bool Success { get; set; }
+        public TData? Data { get; set; }
+        public IEnumerable<Erros>? Erros { get; set; }
+    }
+
+    public class Erros
+    {
+        public string Title { get; set; }
+        public string Code { get; set; }
+        public string Message { get; set; }
+    }
+}
